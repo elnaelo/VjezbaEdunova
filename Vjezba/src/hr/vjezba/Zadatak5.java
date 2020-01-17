dd356fbbe0a4cb3556ab39d75c1861e7d13a6110
Zvonimir Mesinger <zvonimir.mesinger@gmail.com>
2020-01-17 11:55:23 +0100

    Spiralna matrica

--- Vjezba/src/hr/vjezba/Zadatak5.java
+++ Vjezba/src/hr/vjezba/Zadatak5.java
@@ -1,6 +1,6 @@
 package hr.vjezba;
 
-
+import javax.swing.JOptionPane;
 
 public class Zadatak5 {
 	
@@ -8,32 +8,40 @@ public class Zadatak5 {
 		
 		
 
-		int a = 5;
-		int b = 4;
+		int a = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int b = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
 		int mini=0;
 		int minj=0;
 		int maxi=a-1;
 		int maxj=b-1;
+		int[][] matrica = new int [a][b];
 		int broj=1;
 		
-		int[][] matrica = new int [a][b];
-		 {
-		for(int i=maxj;i>=0;i--) {
+		while(broj<=a*b) {
+
+	
+		for(int i=maxj;i>=mini;i--) {
 			matrica[maxi][i]=broj++;
 		}
-		
-		for(int i=maxj;i>=0;i--) {
+
+		for(int i=maxi-1;i>=mini;i--) {
 			matrica[i][minj]=broj++;
+			
 		}
 		
 		for(int i=minj+1;i<=maxj;i++) {
 			matrica[mini][i]=broj++;
 		}
 		
-		for(int i=mini+1;i<maxi;i++) {
+		for(int i=mini+1;i<=maxi-1;i++) {
 			matrica[i][maxj]=broj++;
 		
 		}
+		 minj++;  
+         mini++;          
+         maxj--;
+         maxi--;
+		 }
 		for (int i = 0; i < a; i++)
 	    {
 	        for (int j = 0; j < b; j++)
@@ -42,8 +50,10 @@ public class Zadatak5 {
 	        }
 	         
 	        System.out.println();
-	    }
+	    	}
+		 }
 	}
-	}
-	}
+	
+
+
 
